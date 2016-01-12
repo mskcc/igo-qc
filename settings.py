7b2da139a738adb2d12fa72748893f12d2a1a82c
Nicholas D. Socci <soccin@mskcc.org>
2016-01-11 21:21:26 -0500

    Update URL to igo from toro

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 #settings.py
 import os
 
-# __file__ refers to the file settings.py 
+# __file__ refers to the file settings.py
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
 
 #LIMS version
-LIMS_version = 'toro' #development: 'toro' / production: 'igo'
+#LIMS_version = 'toro' #development: 'toro' / production: 'igo'
+LIMS_version = 'igo'
 
