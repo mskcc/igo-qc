348fba240706f185ec51ddc790486908c7a6e93b
David Streid <david.streid.nyc@gmail.com>
2019-10-08 15:53:29 -0400

    Fixing constant

--- static/seq-qc/src/constants.js
+++ static/seq-qc/src/constants.js
@@ -1,2 +1,1 @@
-// export const CELL_RANGER_APPLICATION = '10XGenomics_GeneExpression';
-export const CELL_RANGER_APPLICATION = 'HumanWholeGenome';
+export const CELL_RANGER_APPLICATION = '10XGenomics_GeneExpression';
