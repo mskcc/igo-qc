50cb99a5c0927aeb85db382013a92b4953dbd884
David Streid <david.streid.nyc@gmail.com>
2019-10-24 17:24:13 -0400

    Adding REACT_APP_STAGE variable to build scripts

--- static/seq-qc/src/config.js
+++ static/seq-qc/src/config.js
@@ -1,16 +1,16 @@
 // TODO - Configure based on env, like https://serverless-stack.com/chapters/environments-in-create-react-app.html
 const dev = {
     IGO_QC: 'http://localhost:9009',            // socket uwsgi is set to serve from
-    NGS_STATS: 'http://localhost:8080',
-    LIMS_REST: 'http://localhost:5007'
+    NGS_STATS: 'http://localhost:8080'
 };
 const prod = {
     IGO_QC: 'http://localhost:9009',                                 // In production, client will be served from server
-    NGS_STATS: 'http://localhost:8080',
-    LIMS_REST: 'https://igolims.mskcc.org:8443/LimsRest'
+    NGS_STATS: 'http://localhost:8080'
 };
 
-const config = prod;
+const config = process.env.REACT_APP_STAGE === 'production'
+    ? prod
+    : dev;
 
 export default {
     ...config
