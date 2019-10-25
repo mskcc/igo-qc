515acca10d64586838709e0d00d2104637a223ce
David Streid <david.streid.nyc@gmail.com>
2019-10-25 11:08:05 -0400

    Removing mocked endpoints

--- static/seq-qc/src/services/igo-qc-service.js
+++ static/seq-qc/src/services/igo-qc-service.js
@@ -19,11 +19,6 @@ const parseResp = (resp) => {
 * Sends service call to retrieve most recent deliveries
 */
 export function getSeqAnalysisProjects() {
-    // TODO
-    return new Promise((resolve) => resolve(seqAnalysisProjects))
-        .then(resp => {return parseResp(resp) })
-        .catch(error => {throw new Error('Unable to fetch Seq Analysis Projects: ' + error) });
-
     return axios
         .get(config.IGO_QC + '/getSeqAnalysisProjects')
         .then(resp => {return parseResp(resp) })
@@ -39,10 +34,6 @@ export function getRequestProjects() {
         .catch(error => { throw new Error('Unable to fetch Request Projects: ' + error) });
 }
 export const getProjectInfo = (projectId) => {
-    return new Promise((resolve) => resolve(projectInfo))
-        .then(getData)
-        .catch(handleError);
-
     return axios.get(`${config.IGO_QC}/projectInfo/${projectId}`)
         .then(getData)
         .catch(handleError)
