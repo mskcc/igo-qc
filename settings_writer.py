ca05c639e0137ba2a91cb55530b66be637f0fd15
David Streid <david.streid.nyc@gmail.com>
2019-10-29 13:54:08 -0400

    Modifying settings.py to read paths dynamically

--- settings_writer.py
+++ settings_writer.py
@@ -7,19 +7,19 @@ if __name__ == "__main__":
         sys.exit()
 
     use_prod_config = (sys.argv[1] == "prod")
-    APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
     if use_prod_config:
         print("Configuration: PROD")
         # The old seq-qc has *.html files written to its static/html & static/pdf directories. We need to take from here
-        APP_STATIC = "/srv/www/sequencing-qc/static"
+        APP_ROOT = "'/srv/www/sequencing-qc' # We grab from the files written to the old site's directory"
     else:
         print("Configuration: DEV")
-        APP_STATIC = os.path.join(APP_ROOT, 'static')
-    FASTQ_PATH = os.path.join(APP_STATIC, 'html/FASTQ/')
-    LIMS_version = 'igo'
+        APP_ROOT = "os.path.dirname(os.path.abspath(__file__))"
 
-    settings = "import os\nAPP_ROOT = '%s'\nAPP_STATIC = '%s'\nFASTQ_PATH = '%s'\nLIMS_version = '%s'" \
-        % (APP_ROOT, APP_STATIC, FASTQ_PATH, LIMS_version)
+    settings = "import os\n"
+    settings += "APP_ROOT=%s\n" % APP_ROOT
+    settings += "APP_STATIC=os.path.join(APP_ROOT, 'static')\n"
+    settings += "FASTQ_PATH=os.path.join(APP_STATIC, 'html/FASTQ/')\n"
+    settings += "LIMS_version='igo'"
 
     # Write file
     f = open("./settings.py", "r+")
