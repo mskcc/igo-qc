cfdfc67deb9bc58745d2316ef35c46dc685facba
David Streid <david.streid.nyc@gmail.com>
2019-11-15 18:30:29 -0500

    Updating settings_writer to use the URL_PREFIX for authentication request

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import os
-APP_ROOT=os.path.dirname(os.path.abspath(__file__))
+APP_ROOT='/srv/www/sequencing-qc' # We grab from the files written to the old site's directory
 APP_STATIC=os.path.join(APP_ROOT, 'static')
 FASTQ_PATH=os.path.join(APP_STATIC, 'html/FASTQ/')
 LIMS_version='igo'
+URL_PREFIX='/run-qc/'
--- settings_writer.py
+++ settings_writer.py
@@ -11,15 +11,18 @@ if __name__ == "__main__":
         print("Configuration: PROD")
         # The old seq-qc has *.html files written to its static/html & static/pdf directories. We need to take from here
         APP_ROOT = "'/srv/www/sequencing-qc' # We grab from the files written to the old site's directory"
+        URL_PREFIX = "'/run-qc/'"
     else:
         print("Configuration: DEV")
         APP_ROOT = "os.path.dirname(os.path.abspath(__file__))"
+        URL_PREFIX = ''
 
     settings = "import os\n"
     settings += "APP_ROOT=%s\n" % APP_ROOT
     settings += "APP_STATIC=os.path.join(APP_ROOT, 'static')\n"
     settings += "FASTQ_PATH=os.path.join(APP_STATIC, 'html/FASTQ/')\n"
-    settings += "LIMS_version='igo'"
+    settings += "LIMS_version='igo'\n"
+    settings += "URL_PREFIX=%s\n" % URL_PREFIX
 
     # Write file
     f = open("./settings.py", "r+")
