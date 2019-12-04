5e1a4c01763ccd3baf1e49a6fb93a3f7f113738e
David Streid <david.streid.nyc@gmail.com>
2019-12-04 12:14:39 -0500

    pointing dev to prod ngs-stats endpoint

--- static/seq-qc/src/config.dev.js
+++ static/seq-qc/src/config.dev.js
@@ -1,6 +1,6 @@
 const config = {
     IGO_QC: 'http://localhost:9009',                         // socket uwsgi is set to serve from
-    NGS_STATS: 'http://localhost:8080',
+    NGS_STATS: 'https://delphi.mskcc.org:8443',
     SITE_HOME: '/'
 };
 export default config;
