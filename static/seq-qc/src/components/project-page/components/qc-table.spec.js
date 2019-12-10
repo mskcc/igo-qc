38353b80de5de65baa240c96929550332b61fb64
David Streid <david.streid.nyc@gmail.com>
2019-12-10 14:04:03 -0500

    Covering tests

--- static/seq-qc/src/components/project-page/components/qc-table.spec.js
+++ static/seq-qc/src/components/project-page/components/qc-table.spec.js
@@ -7,27 +7,69 @@ describe('QcTable', () => {
     let component;
 
     const pid = 'TEST_PID';
+    const data = projectInfo.data.data;
+    const gridData = Object.values(data.grid.grid);
+    const headers = data.grid.header;
+    const columnOrder = data.columnOrder;
+
+    const updatedProps = {
+        data: gridData,
+        headers: headers,
+        qcStatuses: data.statuses,
+        onSelect: () => {},
+        project: pid,
+        recipe: '',
+        addModalUpdate: () => {},
+        updateProjectInfo: () => {},
+        columnOrder: columnOrder
+    };
+    const emptyProps = {
+        data: [],
+        headers: [],
+        qcStatuses: {},
+        onSelect: () => {},
+        project: pid,
+        recipe: '',
+        addModalUpdate: () => {},
+        updateProjectInfo: () => {},
+        columnOrder: []
+    };
 
     beforeEach(() => {
-        const data = projectInfo.data.data.grid;
-        const props = {
-            data: Object.values(data.grid),
-            headers: data.header,
-            qcStatuses: projectInfo.data.data.statuses,
-            onSelect: () => {},
-            project: pid,
-            recipe: '',
-            addModalUpdate: () => {},
-            updateProjectInfo: () => {},
-            columnOrder: projectInfo.data.data.columnOrder
-        };
         act(() => {
-            component = mount(<QcTable {...props}/>);
+            component = mount(<QcTable {...emptyProps}/>);
+            component.setProps(updatedProps);
+
         });
     });
-    it("Project Page passes down props on initialization", () => {
-        // TODO
+    it("QCTable receives filtered properties correctly", () => {
+        component.update();
+        const hotTable = component.find('HotTable').instance();
+        const hotTableProps = hotTable.props;
+
+        // Only headers in columnOrder should be shown
+        expect(hotTableProps.colHeaders).toStrictEqual(columnOrder);
+
+        // All expected entries are available
+        const expectedRows = gridData.map((row) => {return row['QC Record Id']});
+        const actualRows = hotTableProps.data.map((row) => {return row['QC Record Id']});
+        expect(actualRows).toStrictEqual(expectedRows);
     });
 
+
+    it('search filters correctly', () => {
+        component.update();
+        const hotTable = component.find('HotTable').instance();
+        expect(hotTable.props.data.length).toBe(7);
+
+        // Filtering on unique record id should return only one row
+        const evt = {
+            target: {
+                value: '6150444'
+            }
+        };
+        component.instance().runSearch(evt);
+        expect(hotTable.props.data.length).toBe(1);
+    });
 });
 
