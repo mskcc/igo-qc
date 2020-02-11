9504d4ca6558d7270944f2568a6e52042ed413d8
David Streid <david.streid.nyc@gmail.com>
2020-02-11 12:46:58 -0500

    getFlagIcon util

---
+++ static/seq-qc/src/components/project-page/components/quality-checks/quality-checks-utils.js
@@ -0,0 +1,49 @@
+import {CROSSCHECK_METRICS_FLAG_ERROR, CROSSCHECK_METRICS_FLAG_WARNING} from "../../../../resources/constants";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck, faEllipsisH, faExclamationCircle, faExclamationTriangle} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+/**
+ * Does logic to parse out flag information to visualize in table cell
+ *
+ * @param flagField, { FLAG_TYPE: String, ... }
+ * @returns {*}
+ */
+export const getFlagIcon = (flagField) => {
+    if(flagField && Object.keys(flagField).length > 0){
+        const flags = Object.keys(flagField);
+        const errorFlags = flags.filter((f) => {return flagField[f] === CROSSCHECK_METRICS_FLAG_ERROR});
+        const warningFlags = flags.filter((f) => {return flagField[f] === CROSSCHECK_METRICS_FLAG_WARNING});
+
+        if(errorFlags.length > 0){
+            // ERROR
+            return <div className={"flag-container tooltip"}>
+                <FontAwesomeIcon className="em5 mskcc-red" icon={faExclamationCircle}/>
+                <span className={"tooltiptext"}>Checks failed</span>
+                { flags.map((flagType) => {
+                    return <p>{flagType}</p>
+                }) }
+            </div>
+        } else if (warningFlags.length > 0){
+            // WARNING
+            return <div className={"flag-container tooltip"}>
+                <FontAwesomeIcon className="em5 mskcc-dark-yellow" icon={faExclamationTriangle}/>
+                <span className={"tooltiptext"}>Inconclusive results</span>
+                { flags.map((flagType) => {
+                    return <p>{flagType}</p>
+                }) }
+            </div>
+        } else {
+            // Quality Checks Passed
+            return <div className={"flag-container tooltip"}>
+                <FontAwesomeIcon className="em5 mskcc-dark-green" icon={faCheck}/>
+                <span className={"tooltiptext"}>Passed</span>
+            </div>
+        }
+    }
+    // Waiting for results
+    return <div className={"flag-container tooltip"}>
+        <FontAwesomeIcon className="em5 mskcc-medium-blue" icon={faEllipsisH}/>
+        <span className={"tooltiptext"}>No data</span>
+    </div>
+}
--- static/seq-qc/src/components/routers/project_router.js
+++ static/seq-qc/src/components/routers/project_router.js
@@ -16,6 +16,7 @@ import {
     LIMS_REQUEST_ID,
     CROSSCHECK_METRICS_FLAG_ERROR, CROSSCHECK_METRICS_FLAG_WARNING
 } from "../../resources/constants";
+import {getFlagIcon} from "../project-page/components/quality-checks/quality-checks-utils";
 
 // ALL POSSIBLE FIELDS OF ROWS
 const TEXT_FIELDS = {
@@ -95,51 +96,6 @@ const ProjectRouter = (props) => {
             }
         </tr></thead>;
     };
-
-    /**
-     * Does logic to parse out flag information to visualize in table cell
-     *
-     * @param flagField, { FLAG_TYPE: String, ... }
-     * @returns {*}
-     */
-    const getFlagIcon = (flagField) => {
-        if(flagField && Object.keys(flagField).length > 0){
-            const flags = Object.keys(flagField);
-            const errorFlags = flags.filter((f) => {return flagField[f] === CROSSCHECK_METRICS_FLAG_ERROR});
-            const warningFlags = flags.filter((f) => {return flagField[f] === CROSSCHECK_METRICS_FLAG_WARNING});
-
-            if(errorFlags.length > 0){
-                // ERROR
-                return <div className={"flag-container tooltip"}>
-                        <FontAwesomeIcon className="em5 mskcc-red" icon={faExclamationCircle}/>
-                        <span className={"tooltiptext"}>Checks failed. View Project</span>
-                        { flags.map((flagType) => {
-                            return <p>{flagType}</p>
-                        }) }
-                </div>
-            } else if (warningFlags.length > 0){
-                // WARNING
-                return <div className={"flag-container tooltip"}>
-                    <FontAwesomeIcon className="em5 mskcc-dark-yellow" icon={faExclamationTriangle}/>
-                    <span className={"tooltiptext"}>Inconclusive results</span>
-                    { flags.map((flagType) => {
-                        return <p>{flagType}</p>
-                    }) }
-                </div>
-            } else {
-                // Quality Checks Passed
-                return <div className={"flag-container tooltip"}>
-                    <FontAwesomeIcon className="em5 mskcc-dark-green" icon={faCheck}/>
-                    <span className={"tooltiptext"}>Passed</span>
-                </div>
-            }
-        }
-        // Waiting for results
-        return <div className={"flag-container tooltip"}>
-            <FontAwesomeIcon className="em5 mskcc-medium-blue" icon={faEllipsisH}/>
-            <span className={"tooltiptext"}>No data</span>
-        </div>
-    }
 
     const renderProjects = () => {
         const projectElements = [];
