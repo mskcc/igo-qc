f2e6d756811c57711df5d5e5084ad445bdf4e02e
David Streid <david.streid.nyc@gmail.com>
2020-02-20 13:44:55 -0500

    Fixing excel download

--- static/seq-qc/src/components/project-page/components/qc-table.js
+++ static/seq-qc/src/components/project-page/components/qc-table.js
@@ -9,9 +9,7 @@ import {
     faAngleRight,
     faFileExcel
 } from "@fortawesome/free-solid-svg-icons";
-import { BehaviorSubject } from 'rxjs';
-import FileSaver from "file-saver";
-import XLSX from 'xlsx';
+import { BehaviorSubject } from 'rxjs';=
 import config from '../../../config.js';
 
 import 'handsontable/dist/handsontable.full.css'
@@ -351,7 +349,7 @@ class QcTable extends React.Component {
                     <div className={"xlsx-container"}>
                         <div className={"xlsx-selector"}>
                             <div className={"xlsx-selector-inner"}>
-                                <div className={"xlsx-type-selector black-border-right hover"} onClick={this.downloadExcel}>
+                                <div className={"xlsx-type-selector black-border-right hover"} onClick={downloadExcel}>
                                     <p className={"font-bold"}>Table Excel</p>
                                 </div>
                                 <a href={`${config.NGS_STATS}/ngs-stats/get-picard-project-excel/${this.props.project}`}>
