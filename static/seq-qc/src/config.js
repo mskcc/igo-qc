71a91e48b849fa0981f1583448c8e7da64aee0c4
Mirhaj <f.mirhaj@gmail.com>
2022-07-14 09:19:11 -0400

    Moving to http.

--- static/seq-qc/src/config.js
+++ static/seq-qc/src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    IGO_QC: '/run-qc/',                                 // In production, client will be served from server
-    NGS_STATS: 'https://delphi.mskcc.org:8443',
-    SITE_HOME: '/run-qc/',
-    SITE_HELP: '/run-qc/help/'
+    IGO_QC: 'http://localhost:9009',                         // socket uwsgi is set to serve from
+    NGS_STATS: 'http://delphi.mskcc.org:8080',
+    SITE_HOME: '/',
+    SITE_HELP: '/help/'
 };
 export default config;
